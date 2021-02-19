527436273ae63c4130881ee0051d3df2ecdab861
chenxi.sun <scxixixi@foxmail.com>
2021-02-19 11:36:11 +0800

    solving conflict on the second stash

--- demo_master.py
+++ demo_master.py
@@ -1,4 +1,8 @@
 print("Hello, it's the first line.")
 print("This is the first modification")
+<<<<<<< Updated upstream
 # making conflict on dev
 # dev waiting for the first stash
+=======
+# dev waiting for the second stash
+>>>>>>> Stashed changes
